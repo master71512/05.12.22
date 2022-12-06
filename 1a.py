901105d86326e7ec50072b5fb924a68f493e0ac3
dmitriy <master71512@mail.ru>
2022-12-06 20:48:08 +0300

    1b done, 1a changed

--- 1a.py
+++ 1a.py
@@ -8,24 +8,33 @@
 
 # a) Добавьте игру против бота
 
+from random import randint
 N = 120
 max_choice = 28
+print('На столе лежит 120 конфет.\nИграют два игрока, делая ход друг после друга.\n\
+Первый ход делает человек. За один ход можно забрать не более чем 28 конфет.\n\
+Победитель - тот, кто оставил на столе 0 конфет.')
 player = input('Введите имя игрока:\n')
-from random import randint
-while N >0:
+while N > 0:
     if N > 0:
         now_play = player
         print('Конфет:', N)
-        print(f'Сейчас ходит {now_play}, сколько возьмете конфет? (от 1 до 28 включительно)')
+        print(
+            f'Сейчас ходит {now_play}, сколько возьмете конфет? (от 1 до 28 включительно)')
         player_choice = int(input())
-        while player_choice > 28 or player_choice < 0:
-            print('Можно взять только от 0 до 28 конфет, так сколько берете?')
+        while player_choice > N:
+            print(
+                f"Осталось всего {N} конфет, нельзя взять больше! Так сколько берете?")
+            player_choice = int(input())
+        while player_choice > 28 or player_choice < 1:
+            print('Можно взять только от 1 до 28 конфет, так сколько берете?')
             player_choice = int(input())
         N -= player_choice
     if N > 0:
         now_play = 'бот'
         print('Конфет:', N)
-        print(f'Сейчас ходит {now_play}, сколько возьмете конфет? (от 1 до 28 включительно)')
+        print(
+            f'Сейчас ходит {now_play}, сколько возьмете конфет? (от 1 до 28 включительно)')
         if N > max_choice:
             bot_choice = randint(1, max_choice)
         else:
