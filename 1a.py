c207b22d23c48bb958e3f0b89e0d29b7cce8c1b5
dmitriy <master71512@mail.ru>
2022-12-06 20:01:06 +0300

    1a changed

--- 1a.py
+++ 1a.py
@@ -16,12 +16,16 @@ while N >0:
     if N > 0:
         now_play = player
         print('Конфет:', N)
-        print(f'Сейчас ходит {now_play}, сколько возьмете конфет?')
-        N -= int(input())
+        print(f'Сейчас ходит {now_play}, сколько возьмете конфет? (от 1 до 28 включительно)')
+        player_choice = int(input())
+        while player_choice > 28 or player_choice < 0:
+            print('Можно взять только от 0 до 28 конфет, так сколько берете?')
+            player_choice = int(input())
+        N -= player_choice
     if N > 0:
         now_play = 'бот'
         print('Конфет:', N)
-        print(f'Сейчас ходит {now_play}, сколько возьмете конфет?')
+        print(f'Сейчас ходит {now_play}, сколько возьмете конфет? (от 1 до 28 включительно)')
         if N > max_choice:
             bot_choice = randint(1, max_choice)
         else:
